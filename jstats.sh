08584c19fa67ab98319395364d28e9f4c8b301f2
Özgür Kazanççı <4593260+ozgurkazancci@users.noreply.github.com>
2022-08-06 08:31:44 +0300

    Update jstats.sh

--- jstats.sh
+++ jstats.sh
@@ -20,7 +20,7 @@ printf "\n------------------"
 printf "\nJails - RAM usage:"
 printf "\n[kB] - [MB] - [GB]"
 printf "\n------------------\n"
-jls name | while SUM= read -r line; do printf "$line: " && ps -o %mem= -J "$line" | awk '{sum+=$1} END {printf("%.1f%%\n",sum)}' && 
+jls name | while SUM= read -r line; do printf "$line: " && ps -o %mem= -J "$line" | awk '{sum+=$1} END {printf("%.1f%%\n",sum)}' && \
 ps -o rss -J "$line" | awk '{ total = total + $1 } END { printf total " kB - " total/1000 " MB - "  total/1000**2 " GB\n\n" }'; done
 
 printf "\nTotal RAM usage: "
